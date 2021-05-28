57d9cb0e900d4009d30fceffedc53bb0e3eeecfb
coderjones <coderjones@fastmail.com>
2021-05-28 14:51:47 -0500

    Update Chapter4.R

--- Chapter4.R
+++ Chapter4.R
@@ -7,22 +7,22 @@ library(tidyverse)
 # a. Fit linear regression model for the 60s, 70s, 80s, 90s
 
 # 60s 
-my_60_teams <- Teams %>% filter(yearID > 1960) %>% filter(yearID < 1971) %>%
+my_60_teams <- Teams %>% filter(yearID > 1960, year < 1971) %>%
   select(teamID, yearID, lgID, G, W, L, R, RA) %>% 
   mutate(RD = R - RA, Wpct = W / (W + L))
 
 # 70s 
-my_70_teams <- Teams %>% filter(yearID > 1970) %>% filter(yearID < 1981) %>%
+my_70_teams <- Teams %>% filter(yearID > 1970, yearID < 1981) %>%
   select(teamID, yearID, lgID, G, W, L, R, RA) %>% 
   mutate(RD = R - RA, Wpct = W / (W + L))
 
 # 80s 
-my_80_teams <- Teams %>% filter(yearID > 1980) %>% filter(yearID < 1991) %>%
+my_80_teams <- Teams %>% filter(yearID > 1980, yearID < 1991) %>%
   select(teamID, yearID, lgID, G, W, L, R, RA) %>% 
   mutate(RD = R - RA, Wpct = W / (W + L))
 
 # 90s 
-my_90_teams <- Teams %>% filter(yearID > 1990) %>% filter(yearID < 2001) %>%
+my_90_teams <- Teams %>% filter(yearID > 1990, yearID < 2001) %>%
   select(teamID, yearID, lgID, G, W, L, R, RA) %>% 
   mutate(RD = R - RA, Wpct = W / (W + L))
 
@@ -36,15 +36,10 @@ lin70
 lin80
 lin90
 
-p60 <- 0.499933 + (0.000704 * 10)
-p70 <- 0.4999884 + (0.0006375 * 10)
-p80 <- 0.4999448 + (0.0007014 * 10)
-p90 <- 0.4999994 + (0.0006276 * 10)
-
-p60
-p70
-p80
-p90
+predict(lin60, data.frame(RD = 10))
+predict(lin70, data.frame(RD = 10))
+predict(lin80, data.frame(RD = 10))
+predict(lin90, data.frame(RD = 10))
 
 #  This is the book's solution which is better.  In the future, keep this in mind.
 
